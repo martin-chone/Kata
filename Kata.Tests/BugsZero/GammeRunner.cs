8c34f5e4190fd72911d3ec9ebe36f3d23d5930c1
Martin <martinchone@yahoo.fr>
2025-05-12 17:17:21 +0200

    BugsZero : Add GameRunner class

---
+++ Kata.Tests/BugsZero/GammeRunner.cs
@@ -0,0 +1,33 @@
+﻿namespace Kata.Tests.BugsZero
+{
+    public class GameRunner
+    {
+        private static bool notAWinner;
+
+        public static void Main(string[] args)
+        {
+            Game aGame = new Game();
+
+            aGame.AddPlayer("Chet");
+            aGame.AddPlayer("Pat");
+            aGame.AddPlayer("Sue");
+
+            Random rand = new Random();
+
+            do
+            {
+                aGame.Roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.WrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.WasCorrectlyAnswered();
+                }
+            } while (notAWinner);
+
+        }
+    }
+}
