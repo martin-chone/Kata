3770b9d98f70d3ea230155028ee3a6533de05d37
Martin <martinchone@yahoo.fr>
2025-05-15 20:13:58 +0200

    Add PaginationSeven Kata

---
+++ Kata.Tests/PaginationSeven/Pagination.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Kata.Tests.PaginationSeven
+{
+    public static class Pagination
+    {
+        public static string GetSeven(int current, int total)
+        {
+            if (total > 7) 
+            {
+                if(current < 5) return $"{Format(current, 1, 5)} ... {total}";
+
+                if (current > total - 4)
+                    return $"1 ... {Format(current, total - 4, total)}";
+
+                return $"1 ... {current - 1} ({current}) {current + 1} ... {total}"; 
+            }
+
+            return Format(current, 1, total);
+        }
+
+        private static string Format(int current, int from, int to)
+        {
+            IList<string> parts = new List<string>();
+            for (int i = from; i <= to; i++)
+                parts.Add((i == current) ? $"({i})" : $"{i}");
+
+            return string.Join(" ", parts);
+        }
+    }
+}
---
+++ Kata.Tests/PaginationSeven/PaginationSevenTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Kata.Tests.PaginationSeven
+{
+    public class PaginationSevenTests
+    {
+        [Theory]
+        [InlineData(1, 7, "(1) 2 3 4 5 6 7")]
+        [InlineData(2, 7, "1 (2) 3 4 5 6 7")]
+        [InlineData(3, 7, "1 2 (3) 4 5 6 7")]
+        [InlineData(6, 7, "1 2 3 4 5 (6) 7")]
+        [InlineData(2, 5, "1 (2) 3 4 5")]
+        [InlineData(5, 9, "1 ... 4 (5) 6 ... 9")]
+        [InlineData(42, 100, "1 ... 41 (42) 43 ... 100")]
+        [InlineData(2, 9, "1 (2) 3 4 5 ... 9")]
+        [InlineData(4, 9, "1 2 3 (4) 5 ... 9")]
+        [InlineData(6, 9, "1 ... 5 (6) 7 8 9")]
+        [InlineData(8, 9, "1 ... 5 6 7 (8) 9")]
+        [InlineData(102, 102, "1 ... 98 99 100 101 (102)")]
+        public void ShouldCurrentPageOfTotalPages(int input, int total, string expected)
+        {
+            Assert.Equal(expected, Pagination.GetSeven(input, total));
+        }
+    }
+}
